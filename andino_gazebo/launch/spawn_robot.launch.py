e8b39b9c3c512c5d199ed16e1f0d0d3c2f85a1a2
olmerg <olmerg@gmail.com>
2023-07-10 14:43:50 -0500

    single quote flake8
    Signed-off-by: olmerg <olmerg@gmail.com>

--- andino_gazebo/launch/spawn_robot.launch.py
+++ andino_gazebo/launch/spawn_robot.launch.py
@@ -185,16 +185,16 @@ def generate_launch_description():
         ],
     )
     joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster', '--controller-manager', '/controller_manager'],
         condition=IfCondition(use_ros_control),
     )
 
     diff_drive_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["diff_controller", "--controller-manager", "/controller_manager"],
+        package='controller_manager',
+        executable='spawner',
+        arguments=['diff_controller', '--controller-manager', '/controller_manager'],
         condition=IfCondition(use_ros_control),
     )
 
