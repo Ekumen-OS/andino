e513150703962e8d723fb7870a3e8bd1cf3b982a
olmerg <olmerg@gmail.com>
2023-07-11 08:28:23 -0500

    refactor the code, removing duplicate assets
    Signed-off-by: olmerg <olmerg@gmail.com>

--- andino_navigation/launch/andino_simulation_navigation_launch.py
+++ andino_navigation/launch/andino_simulation_navigation_launch.py
@@ -34,65 +34,25 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import (
-    DeclareLaunchArgument,
-    ExecuteProcess,
-    IncludeLaunchDescription,
-)
-from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     # Get the launch directory
-    bringup_dir = get_package_share_directory('nav2_bringup')
+    nav2_bringup_dir = get_package_share_directory('nav2_bringup')
     andino_navigation_dir = get_package_share_directory('andino_navigation')
-    nav2_launch_dir = os.path.join(bringup_dir, 'launch')
+    andino_gazebo_dir = get_package_share_directory('andino_gazebo')
+
     # This checks that tb3 exists needed for the URDF. If not using TB3, its safe to remove.
     _ = get_package_share_directory('turtlebot3_gazebo')
-    pkg_andino_gazebo = get_package_share_directory('andino_gazebo')
 
     # Create the launch configuration variables
-    slam = LaunchConfiguration('slam')
-    namespace = LaunchConfiguration('namespace')
-    use_namespace = LaunchConfiguration('use_namespace')
-    map_yaml_file = LaunchConfiguration('map')
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
-    autostart = LaunchConfiguration('autostart')
-    use_composition = LaunchConfiguration('use_composition')
-    use_respawn = LaunchConfiguration('use_respawn')
-
-    # Launch configuration variables specific to simulation
-    rviz_config_file = LaunchConfiguration('rviz_config_file')
-    use_simulator = LaunchConfiguration('use_simulator')
-    use_rviz = LaunchConfiguration('use_rviz')
-    headless = LaunchConfiguration('headless')
-    world = LaunchConfiguration('world')
 
     # Declare the launch arguments
-    declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace', default_value='', description='Top-level namespace'
-    )
-
-    declare_use_namespace_cmd = DeclareLaunchArgument(
-        'use_namespace',
-        default_value='false',
-        description='Whether to apply a namespace to the navigation stack',
-    )
-
-    declare_slam_cmd = DeclareLaunchArgument(
-        'slam', default_value='False', description='Whether run a SLAM'
-    )
-
-    declare_map_yaml_cmd = DeclareLaunchArgument(
-        'map',
-        default_value=os.path.join(
-            andino_navigation_dir, 'maps', 'turtlebot3_world.yaml'
-        ),
-        description='Full path to map file to load',
-    )
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
@@ -106,96 +66,30 @@ def generate_launch_description():
         description='Full path to the ROS2 parameters file to use for all launched nodes',
     )
 
-    declare_autostart_cmd = DeclareLaunchArgument(
-        'autostart',
-        default_value='true',
-        description='Automatically startup the nav2 stack',
-    )
-
-    declare_use_composition_cmd = DeclareLaunchArgument(
-        'use_composition',
-        default_value='True',
-        description='Whether to use composed bringup',
-    )
-
-    declare_use_respawn_cmd = DeclareLaunchArgument(
-        'use_respawn',
-        default_value='False',
-        description='Whether to respawn if a node crashes. Applied when composition is disabled.',
-    )
-
-    declare_rviz_config_file_cmd = DeclareLaunchArgument(
-        'rviz_config_file',
-        default_value=os.path.join(
-            andino_navigation_dir, 'rviz', 'nav2_default_view.rviz'
-        ),
-        description='Full path to the RVIZ config file to use',
-    )
-
-    declare_use_simulator_cmd = DeclareLaunchArgument(
-        'use_simulator',
-        default_value='True',
-        description='Whether to start the simulator',
-    )
-
-    declare_use_rviz_cmd = DeclareLaunchArgument(
-        'use_rviz', default_value='True', description='Whether to start RVIZ'
-    )
-
-    declare_simulator_cmd = DeclareLaunchArgument(
-        'headless', default_value='False', description='Whether to execute gzclient)'
-    )
-
-    declare_world_cmd = DeclareLaunchArgument(
-        'world',
-        default_value=os.path.join(bringup_dir, 'worlds', 'world_only.model'),
-        description='Full path to world model file to load',
-    )
-
-    # Specify the actions
-    start_gazebo_server_cmd = ExecuteProcess(
-        condition=IfCondition(use_simulator),
-        cmd=[
-            'gzserver',
-            '-s',
-            'libgazebo_ros_init.so',
-            '-s',
-            'libgazebo_ros_factory.so',
-            world,
-        ],
-        cwd=[andino_navigation_dir],
-        output='screen',
-    )
-
-    start_gazebo_client_cmd = ExecuteProcess(
-        condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])),
-        cmd=['gzclient'],
-        cwd=[andino_navigation_dir],
-        output='screen',
-    )
-
-    # add spawn andino
-    # Include andino
+    # Include andino simulation
     include_andino = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_andino_gazebo, 'launch', 'spawn_robot.launch.py')
+            os.path.join(andino_gazebo_dir, 'launch', 'andino_one_robot.launch.py')
         ),
         launch_arguments={
-            'use_gazebo_ros_control': 'false',
+            'use_gazebo_ros_control': LaunchConfiguration(
+                'use_gazebo_ros_control', default='false'
+            ),
             'initial_pose_x': LaunchConfiguration('initial_pose_x', default='-2.00'),
             'initial_pose_y': LaunchConfiguration('initial_pose_y', default='-0.50'),
             'initial_pose_z': LaunchConfiguration('initial_pose_z', default='0.01'),
             'initial_pose_yaw': LaunchConfiguration('initial_pose_yaw', default='0.00'),
-        }.items(),
-    )
-
-    rviz_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(nav2_launch_dir, 'rviz_launch.py')),
-        condition=IfCondition(use_rviz),
-        launch_arguments={
-            'namespace': namespace,
-            'use_namespace': use_namespace,
-            'rviz_config': rviz_config_file,
+            'world': LaunchConfiguration(
+                'world',
+                default=os.path.join(nav2_bringup_dir, 'worlds', 'world_only.model'),
+            ),
+            'rviz_config_file': LaunchConfiguration(
+                'rviz_config_file',
+                default=os.path.join(
+                    andino_navigation_dir, 'rviz', 'nav2_default_view.rviz'
+                ),
+            ),
+            'use_sim_time': use_sim_time,
         }.items(),
     )
 
@@ -204,46 +98,23 @@ def generate_launch_description():
             os.path.join(andino_navigation_dir, 'launch', 'bringup_launch.py')
         ),
         launch_arguments={
-            'namespace': namespace,
-            'use_namespace': use_namespace,
-            'slam': slam,
-            'map': map_yaml_file,
+            'map': LaunchConfiguration(
+                'map',
+                default=os.path.join(
+                    andino_navigation_dir, 'maps', 'turtlebot3_world.yaml'
+                ),
+            ),
             'use_sim_time': use_sim_time,
             'params_file': params_file,
-            'autostart': autostart,
-            'use_composition': use_composition,
-            'use_respawn': use_respawn,
         }.items(),
     )
 
-    # Create the launch description and populate
     ld = LaunchDescription()
 
-    # Declare the launch options
-    ld.add_action(declare_namespace_cmd)
-    ld.add_action(declare_use_namespace_cmd)
-    ld.add_action(declare_slam_cmd)
-    ld.add_action(declare_map_yaml_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_params_file_cmd)
-    ld.add_action(declare_autostart_cmd)
-    ld.add_action(declare_use_composition_cmd)
 
-    ld.add_action(declare_rviz_config_file_cmd)
-    ld.add_action(declare_use_simulator_cmd)
-    ld.add_action(declare_use_rviz_cmd)
-    ld.add_action(declare_simulator_cmd)
-    ld.add_action(declare_world_cmd)
-
-    ld.add_action(declare_use_respawn_cmd)
-
-    # Add any conditioned actions
-    ld.add_action(start_gazebo_server_cmd)
-    ld.add_action(start_gazebo_client_cmd)
-
-    # Add the actions to launch all of the navigation nodes
     ld.add_action(include_andino)
-    ld.add_action(rviz_cmd)
     ld.add_action(bringup_cmd)
 
     return ld
