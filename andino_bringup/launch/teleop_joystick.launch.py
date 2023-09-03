90654d5c1619e2a88f71d292c362bbf27a93ae76
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2023-09-03 16:50:03 -0300

    Provides info about joy teleoperation. (#142)
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- andino_bringup/launch/teleop_joystick.launch.py
+++ andino_bringup/launch/teleop_joystick.launch.py
@@ -32,11 +32,19 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
     joystick_config = os.path.join(get_package_share_directory('andino_bringup'),'config','joystick.yaml')
+
+    cmd_vel_topic_arg = DeclareLaunchArgument(
+            'cmd_vel_topic',
+            default_value='/cmd_vel_joy',
+            description='Indicates the cmd_vel topic.')
+    cmd_vel_topic =  LaunchConfiguration('cmd_vel_topic')
 
     joy_linux_node = Node(
             package='joy_linux',
@@ -49,10 +57,11 @@ def generate_launch_description():
             executable='teleop_node',
             name='teleop_node',
             parameters=[joystick_config],
-            remappings=[('/cmd_vel','/cmd_vel_joy')]
+            remappings=[('/cmd_vel', cmd_vel_topic)]
          )
 
     return LaunchDescription([
+        cmd_vel_topic_arg,
         joy_linux_node,
         teleop_node,
     ])
