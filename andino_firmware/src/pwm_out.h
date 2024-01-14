211b8619b686c1c46c9e4768e8a295f13d5c9c60
Javier Balloffet <javier.balloffet@gmail.com>
2024-01-14 20:31:47 +0100

    Add PWM output interface and the Arduino implementation
    Signed-off-by: Javier Balloffet <javier.balloffet@gmail.com>

---
+++ andino_firmware/src/pwm_out.h
@@ -0,0 +1,59 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2024, Ekumen Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+namespace andino {
+
+/// @brief This class defines an interface for PWM outputs.
+class PwmOut {
+ public:
+  /// @brief Constructs a PwmOut using the specified GPIO pin.
+  ///
+  /// @param gpio_pin GPIO pin.
+  explicit PwmOut(const int gpio_pin) : gpio_pin_(gpio_pin) {}
+
+  /// @brief Destructs the PWM output.
+  virtual ~PwmOut() {}
+
+  /// @brief Initializes the PWM output.
+  virtual void begin() const = 0;
+
+  /// @brief Sets the PWM output value (0 to 255).
+  ///
+  /// @param value PWM value.
+  // TODO(jballoffet): Change this API to expect values from 0 to 100 (%).
+  virtual void write(int value) const = 0;
+
+ protected:
+  /// GPIO pin.
+  const int gpio_pin_;
+};
+
+}  // namespace andino
