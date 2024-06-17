dae849e2c3b9d2340bce3dd14c1bc61ef3d8178a
Jesús Silva <79662866+JesusSilvaUtrera@users.noreply.github.com>
2024-06-17 09:51:59 +0200

    Jesus/#169 camera info yaml (#231)
    * Improves andino_navigation package. (#224)
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Add basic Platform IO instructions (#229)
    
    Signed-off-by: Gary Servin <gary@servin.dev>
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Modify Shell class API to allow dependency injection (#221)
    
    Signed-off-by: Javier Balloffet <javier.balloffet@gmail.com>
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Fixed wheel diameter in URDF (#93)
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Added YAML file for default camera intrinsics (#169)
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Adds references to o3de and isaac_sim simulations. (#232)
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Added README for andino_bringup (#233)
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Added binaries installation docs (#234)
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * andino_apps package created and andino_navigation package updated (#235)
    
    * andino_apps package created and andino_navigation package updated
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Minor changes from the PR
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Update andino_navigation README with changes from PR
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Add andino_apps package to ci.yaml
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Fixed minor issues from the PR
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Added 'andino_apps' to the general README
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    ---------
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Minimal change to test launch file
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Added webcam.yaml file and intrinsics are now publishing
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Added raspicam.yaml with camera intrinsics and updated instructions in andino_hardware/README
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    * Changed the 'camera_info_url' to be relative path
    
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    
    ---------
    
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>
    Signed-off-by: Gary Servin <gary@servin.dev>
    Signed-off-by: Javier Balloffet <javier.balloffet@gmail.com>
    Co-authored-by: Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
    Co-authored-by: Gary Servin <gary@servin.dev>
    Co-authored-by: Javier Balloffet <javier.balloffet@gmail.com>
    Signed-off-by: JesusSilvaUtrera <jsilvautrera@gmail.com>

--- andino_bringup/launch/camera.launch.py
+++ andino_bringup/launch/camera.launch.py
@@ -30,17 +30,32 @@
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
+from ament_index_python.packages import get_package_share_directory
+from os.path import join
+
+pkg_andino_bringup = get_package_share_directory('andino_bringup')
 
 def generate_launch_description():
-    return LaunchDescription([
+    # Declare launch argument for the path to the camera params YAML file (the 'file://' part is mandatory, you can't skip it)
+    intrinsic_params_file = DeclareLaunchArgument(
+        'intrinsic_params_file',
+        default_value='file://' + join(pkg_andino_bringup, 'config', 'raspicam.yaml'),
+        description='Path to camera intrinsics YAML file'
+    )
 
+    return LaunchDescription([
+        intrinsic_params_file,
         Node(
             package='v4l2_camera',
             executable='v4l2_camera_node',
+            name='v4l2_camera_node',
             output='screen',
             parameters=[{
-                'image_size': [640,480],
-                'camera_frame_id': 'camera_link'
-                }]
-    )
+                'image_size': [640, 480],
+                'camera_frame_id': 'camera_link',
+                'camera_info_url': LaunchConfiguration('intrinsic_params_file'),
+            }],
+        )
     ])
