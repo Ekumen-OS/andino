033e0eb70c495dc29b90d23dbd7f72169b7ea8e9
Javier Balloffet <javier.balloffet@gmail.com>
2026-08-16 13:46:50 +0200

    Remove unused read analog GPIO command
    Signed-off-by: Javier Balloffet <javier.balloffet@gmail.com>

--- andino_firmware/include/andino/app/commands.h
+++ andino_firmware/include/andino/app/commands.h
@@ -68,8 +68,6 @@ namespace andino {
 
 /// @brief CLI commands.
 struct Commands {
-  /// @brief Reads an analog GPIO.
-  static constexpr const char* kReadAnalogGpio{"a"};
   /// @brief Reads an encoder digital input value [format: "encoder channel", encoder: 0 (left) or
   /// 1 (right), channel: 0 (A) or 1 (B)].
   static constexpr const char* kReadDigitalGpio{"d"};
